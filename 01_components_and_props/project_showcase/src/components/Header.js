b9fe6785344a07eef7232f830d85d7eadf5049b3
TR-Wolf <thomasrussellwolf@gmail.com>
2023-07-31 13:16:36 -0500

    lecture complete

---
+++ 01_components_and_props/project_showcase/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <h1>
+                <span className="logo">{"//"}</span>Project showcase
+            </h1>
+        </header>
+    )
+}
+
+export default Header;
